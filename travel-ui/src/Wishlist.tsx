70ce29743440255205812d87f666d593ccb61917
Kelli Lung <ksl3618@rit.edu>
2025-06-20 15:15:53 +0200

    set up necessary ui files for frontend work

---
+++ travel-ui/src/Wishlist.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+
+function Wishlist() {
+  const [wishlist, setWishlist] = useState([]);
+  const [destination, setDestination] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/wishlist')
+      .then(res => res.json())
+      .then(data => setWishlist(data));
+  }, []);
+
+  const addDestination = () => {
+    fetch('http://localhost:5000/api/wishlist', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ destination }),
+    })
+      .then(res => res.json())
+      .then(data => setWishlist(data.wishlist));
+    setDestination('');
+  };
+
+  return (
+    <div>
+      <input
+        value={destination}
+        onChange={e => setDestination(e.target.value)}
+        placeholder="Add destination"
+      />
+      <button onClick={addDestination}>Add</button>
+      <ul>
+        {wishlist.map((place, idx) => <li key={idx}>{place}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default Wishlist;
